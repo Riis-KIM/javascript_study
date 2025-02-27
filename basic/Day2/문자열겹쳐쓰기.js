fd3124d78f6710936dcdda3cb56f07ba78545510
Riis <gunil099@gmail.com>
2025-02-27 17:05:30 +0900

    2일차

---
+++ basic/Day2/문자열겹쳐쓰기.js
@@ -0,0 +1,13 @@
+function solution(my_string, overwrite_string, s) {
+    var answer = '';
+    for (let i = 0; i <s; i++){
+        answer += my_string[i]
+    }
+    answer += overwrite_string
+
+    for (let i = s + overwrite_string.length; i < my_string.length; i++){
+        answer += my_string[i]
+    }
+
+    return answer;
+}
